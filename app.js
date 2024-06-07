afb194bc97a784e77b405b1426a45faac12207d8
marcinzygan <soul@o2.pl>
2024-06-07 14:21:02 +0200

    added handler for unhandled routes

--- app.js
+++ app.js
@@ -11,15 +11,27 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use(express.json());
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   console.log(req.requestTime);
+//   next();
+// });
+
 // ROUTE HANDLERS
 
 //// ROUTES
 
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/users", userRouter);
+
+//// UNHANDLED ROUTES
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server. Please check documentation for available routes.`,
+  });
+  next();
+});
 
 module.exports = app;
