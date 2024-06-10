007ff11cac67e971f52c93aeb31af4e30f9a1a61
marcinzygan <soul@o2.pl>
2024-06-10 07:50:00 +0200

    Implemented a Global Error Handling Middleware

--- app.js
+++ app.js
@@ -27,11 +27,26 @@ app.use("/api/v1/users", userRouter);
 
 //// UNHANDLED ROUTES
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on this server. Please check documentation for available routes.`,
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on this server. Please check documentation for available routes.`,
+  // });
+  const err = new Error(
+    `Can't find ${req.originalUrl} on this server. Please check documentation for available routes.`,
+  );
+  err.status = "fail";
+  err.statusCode = 400;
+  next(err);
+});
+//// ERROR HANDLING MIDDLEWARE
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err.message,
   });
   next();
 });
-
 module.exports = app;
