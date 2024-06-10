5c8e462b926e47ddcbb3ad5527985d32d8c84cd9
marcinzygan <soul@o2.pl>
2024-06-10 14:34:22 +0200

    refactored errorController

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,12 +1,46 @@
+// SEND ERROR IN DEVELOPMENT
+
+const sendErrorDev = function (err, res) {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+// SEND ERROR IN PRODUCTION
+
+const sendErrorProduction = function (err, res) {
+  // Operational trusted error : send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // Programming or unknown error : don't want to leak error details to client
+
+    //1) Log error
+    console.err("ERROR:", err);
+
+    //2) Send generic message
+    res.status(500).json({
+      status: "error",
+      message: "There was an error",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   //   console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err.message,
-    at: err.stack,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProduction(err, res);
+  }
+
   next();
 };
