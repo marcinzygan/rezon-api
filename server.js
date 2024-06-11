a61f9c71654466773887cbb9c1ebd1cd6ac7e005
marcinzygan <soul@o2.pl>
2024-06-11 09:12:44 +0200

    added error handler for unhandled rejections

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ mongoose.connect(DB, {}).then(() => {
 
 const port = process.env.PORT || 8000;
 // APP START
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port} ...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("There was an Error:", err.name, err.message);
+  console.log("🔥 The APP will shut down .... 🔥");
+  server.close(() => {
+    process.exit(1);
+  });
 });
