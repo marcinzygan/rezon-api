4c82ca7d1542ffcf9a9b5e530c73bea370076ccf
marcinzygan <soul@o2.pl>
2024-07-10 08:42:35 +0200

    changed api limit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "development") {
 
 // LIMIT REQUEST FROM SAME IP
 const limiter = rateLimit({
-  max: 100,
+  max: 10000,
   windowMs: 60 * 60 * 1000, //1hr
   message: "Too many request with this IP. Please try again in an hour",
 });
