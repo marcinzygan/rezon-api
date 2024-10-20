67b87cf627c083653952c15c826407762ab29198
marcinzygan <soul@o2.pl>
2024-10-20 18:49:35 +0200

    updated cors

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
     // domain: "rezon.eu",
-    sameSite: "none",
+    // sameSite: "none",
     // httpOnly: true,
     partitioned: true,
   };
